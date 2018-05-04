15bfdf106c64ddf2a3b495dc449e410f81fb23df
m0pjiex <trolologhost3@gmail.com>
2018-05-05 06:07:48 +0700

    Test commit

--- main.cpp
+++ main.cpp
@@ -2,4 +2,14 @@
 using namespace std;
 
 int main(){
+cout <<"Введите число: ";
+int a;
+cin >> a;
+cout << endl;
+cout <<"Введите число: ";
+int b;
+cin >> b;
+cout << endl;
+int c = a + b;
+cout << c << endl;
 }
