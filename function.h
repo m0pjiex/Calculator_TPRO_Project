cce77b87e7e552b41caf47d16c47879ed86c648f
m0pjiex <trolologhost3@gmail.com>
2018-05-07 11:55:29 +0700

    Adding features

--- function.h
+++ function.h
@@ -1,1 +1,15 @@
-.
+#pragma once
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include "Function.h"
+
+void Error();       
+void Sum();         
+void Sub();         
+void Mul();         
+void Div();         
+void Sqrt();        
+void Radical();     
+void Power();  
